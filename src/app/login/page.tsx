776fe48d7fc927be008ae9f758613380354dea59
AlexisSM377 <al222111377@gmail.com>
2024-11-29 00:20:08 -0600

    update provider google

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,12 +5,12 @@ function page() {
   return (
     <div className="flex justify-center items-center h-screen">
       <div>
-        <h1>Login con Github</h1>
+        <h1>Login con Google</h1>
 
         <button
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
           onClick={async () => {
-            const result = await signIn("github", {
+            const result = await signIn("google", {
               callbackUrl: "/dashboard",
               redirect: false,
             });
